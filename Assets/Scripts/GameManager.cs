c97390c3a4435d5f35f5ad02c361746db268e3d9
David Fiorin <david94lozoo@gmail.com>
2017-05-24 11:33:17 +0200

    - Caricato primo protitpo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Days
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+public class GameManager : MonoBehaviour
+{
+    Days today;
+
+	private void Start ()
+    {
+        today = Days.Monday;
+        Debug.Log(today);
+	}
+	
+	private void Update ()
+    {
+		
+	}
+
+    public void SwitchDays()
+    {
+        if (today != Days.Sunday)
+        {
+            today += 1;
+            Debug.Log(today);
+        }
+        else
+        {
+            today = Days.Monday;
+            Debug.Log(today);
+        }
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    /// <summary>
+    /// Facendo il GetComponentInChildren su questo oggetto non mi trova il componente Image figlio... Bug???
+    /// </summary>
+    public Image iconImage;
+    public Image pokeIcon;
+    public Image[] imageArray;
+   
+    private void Awake()
+    {
+        iconImage = this.GetComponent<Image>();
+        imageArray = GetComponentsInChildren<Image>();
+        imageArray[1].enabled = true;
+        pokeIcon = imageArray[1];
+
+        //Debug.Log(iconImage.name);
+
+        /*for (int i = 0; i < imageArray.Length - 1; i++)
+        {
+            Debug.Log(imageArray[1].name);
+        }*/
+
+        pokeIcon.transform.localScale = Vector3.zero;
+    }
+
+    public void Tap()
+    {
+        StartCoroutine(ReduceIconCO(iconImage));
+        StartCoroutine(SpawnItemCO(pokeIcon));
+        StartCoroutine(SpawnItemCO(iconImage));
+
+        iconImage.enabled = false;
+        pokeIcon.enabled = true;
+    }
+
+    private IEnumerator ReduceIconCO(Image _icon)
+    {
+        while (_icon.transform.localScale != Vector3.zero)
+        {
+            _icon.transform.localScale -= new Vector3(.1f, .1f, .1f);            
+            yield return null;
+        }
+    } 
+
+    private IEnumerator SpawnItemCO(Image _icon)
+    {
+        while (_icon.transform.localScale != Vector3.one)
+        {
+            _icon.transform.localScale += new Vector3(.1f, .1f, .1f);
+            yield return null;
+        }
+    }
+}
